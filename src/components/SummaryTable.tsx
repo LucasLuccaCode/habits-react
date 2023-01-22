25e276692d08b982290d59f9ac25d79fda65da5b
Lucca Santos <lucasluccad.r@outlook.com>
2023-01-22 20:06:00 -0300

    feature: feature to create new habit implemented

--- src/components/SummaryTable.tsx
+++ src/components/SummaryTable.tsx
@@ -9,7 +9,6 @@ export function SummaryTable() {
 
   const minimumSummaryDatesSize = 18 * 7 // 18 weeks
   const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length
-  console.log(amountOfDaysToFill)
 
   return (
     <div className="w-full flex">
@@ -26,13 +25,15 @@ export function SummaryTable() {
 
       <div className="grid grid-rows-7 grid-flow-col gap-3">
 
-        {summaryDates.map(date => (
-          <HabitDay 
-            key={date.toString()} 
-            amount={5} 
-            completed={Math.round(Math.random() * 5)} 
-          />
-        ))}
+        {
+          summaryDates.map(date => (
+            <HabitDay
+              key={date.toString()}
+              amount={5}
+              completed={Math.round(Math.random() * 5)}
+            />
+          ))
+        }
 
         {amountOfDaysToFill && Array.from({ length: amountOfDaysToFill }).map((_, index) => (
           <div key={index} className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed" />
